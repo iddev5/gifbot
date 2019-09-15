b911acbe97817c68a85d1c9f91c69562c2b889a9
AyushGameDev <ayushtechportal@gmail.com>
2019-09-15 12:26:50 +0530

    Added command

---
+++ command/g_thatsgay.py
@@ -0,0 +1,11 @@
+import discord
+
+ID = 'GAY'
+
+Title = ['thats gay!!', 'gayeeeee....']
+
+Color = [discord.Color.blue()]
+
+List = [
+    'https://media0.giphy.com/media/GjqgnbGhNPx0A/giphy.gif'
+]
